32751329ffa78607b73033fac61d1c3fbaf1a7e3
micahhowe <micahhowe182@gmail.com>
2019-06-28 16:15:45 -0600

    2 For-in problems complete

--- for-in-delete.js
+++ for-in-delete.js
@@ -11,13 +11,13 @@
   In the example below, we are accessing the property values. Uncomment the code below, run it and look at what prints in the console.
 */
 
-// var values = {
-//   one: 'These',
-//   two: ' are',
-//   three: ' the',
-//   four: ' property',
-//   five: ' values.'
-// } 
+var values = {
+  one: 'These',
+  two: ' are',
+  three: ' the',
+  four: ' property',
+  five: ' values.'
+} 
 
 // for(var key in values) {
 //   console.log(values[key])
@@ -38,12 +38,15 @@
 /*
   Inside the function showValues, write a for in loop that concatenates each of the property values and returns the concatenated string.
 */
-
 function showValues( obj ) {
-  //Code Here
+  let emptyString = ''
+  for (let key in obj){
+    emptyString += obj[key]
+  }
+  return emptyString
 }
 
-
+//console.log(showValues(values))
 
 ////////// PROBLEM 2 //////////
 
@@ -54,7 +57,14 @@ function showValues( obj ) {
 */
 
 //Code Here
-
+function greaterThan10(obj){
+  for (let key in obj){
+    if(obj[key] > 10) {
+      obj[key] = 0
+    }
+  }
+  return obj;
+}
 
 
 ////////// PROBLEM 3 //////////
